3b775a11f198fa233dfa8ec88313ac6639eca3a8
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-05-14 09:38:21 +0000

    Add can_edit and can_delete functions to review model, build comment model and add python comments to both models

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,13 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
-RATING = ((0), (1), (2), (3), (4), (5))
+RATING = ((0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
 
 
 class Review(models.Model):
+    """
+    Model for review
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=150, unique=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="review_posts")
@@ -20,10 +23,58 @@ class Review(models.Model):
     likes = models.ManyToManyField(User, related_name='review_likes', blank=True)
 
     class meta:
+        """
+        Order the reviews in descending order.
+        """
         ordering = ['-created_on']
 
     def __str__(self):
+        """
+        Returns a string showing the title.
+        """
         return self.title
 
     def number_of_likes(self):
+        """
+        See number of likes on a recipe.
+        """
         return self.likes.count()
+
+    def can_edit(self, request, slug):
+        """
+        Allows creator to edit review.
+        """
+        if self.creator:
+            return True
+        else:
+            return False
+
+    def can_delete(self, request, slug):
+        """
+        Allows author to delete review.
+        """
+        if self.creator:
+            return True
+        else:
+            return False
+
+
+class Comment(models.Model):
+    """
+    Model for comments
+    """
+    post = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta: 
+        """
+        Order the comments in ascending order (oldest first)
+        """
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
