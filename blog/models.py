4f0d3f82da7e2bd224cc30b72fc58f199cd8879a
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-05-14 08:36:31 +0000

    Build initial Review model in models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+RATING = ((0), (1), (2), (3), (4), (5))
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="review_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(choices=RATING, default=0)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='review_likes', blank=True)
+
+    class meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
