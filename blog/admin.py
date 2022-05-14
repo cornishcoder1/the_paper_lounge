b3dcdeebd0e3239b83fad2514b21709a35a183e1
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-05-14 09:59:22 +0000

    Install django  superuser and summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Review
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
+
+
