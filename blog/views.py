9f44292083c43ecec54ef99dd6f0b373e153ff0b
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-05-20 13:45:51 +0000

    Add like functionality - class based view 'ReviewLike' in views.py(lines 69-79), add relevant code to review_detail.html (lines 41-70)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Review
 from .forms import CommentForm
 
@@ -63,3 +64,18 @@ class ReviewDetail(View):
                 "comment_form": CommentForm()
             }
         )
+
+
+class ReviewLike(View):
+
+    def post(self, request, slug):
+        review = get_object_or_404(Review, slug=slug)
+
+        if review.likes.filter(id=request.user.id).exists():
+            review.likes.remove(request.user)
+        else:
+            review.likes.add(request.user)
+        
+        return HttpResponseRedirect(reverse('review_detail', args=[slug]))
+
+         
