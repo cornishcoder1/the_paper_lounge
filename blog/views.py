c35744d9950ea471f4562e67390e052900bca7d7
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-05-20 12:44:33 +0000

    Add post method to ReviewDetail class in views.py (lines 35 - 65). Add 'comment awaiting approval' alert to review_detail.html(lines 85-89)

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,39 @@ class ReviewDetail(View):
             {
                 "review": review,
                 "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            }
+        )
+        
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Review.objects.filter(status=1)
+        review = get_object_or_404(queryset, slug=slug)
+        comments = review.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if review.likes.filter(id=self.request.user.id).exists():
+            liked = True 
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = review
+            comment.save()
+
+        else: 
+            comment_form = CommentForm()
+        
+        return render(
+            request,
+            "review_detail.html",
+            {
+                "review": review,
+                "comments": comments,
+                "commented": True,
                 "liked": liked,
                 "comment_form": CommentForm()
             }
