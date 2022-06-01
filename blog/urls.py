a48766eca79979c6c22596bf44399586ef367b89
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-06-01 11:27:55 +0000

    Add delete modal to review_detail.html

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
     path('add-review/', views.add_review, name='add_review'),
     path('edit-review/<slug:slug>', views.edit_review, name='edit_review'),
-    # path('delete-review/<slug:slug>', views.delete_review,
-    #      name='delete_review'),
     path('delete-review/<slug:slug>', views.delete_review, name='delete_review'),
     path('reviews/<slug:slug>/', views.ReviewDetail.as_view(), name='review_detail'),
     path('like/<slug:slug>)', views.ReviewLike.as_view(), name='review_like'),
