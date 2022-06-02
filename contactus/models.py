c23484e99f101121b63b7148437f5961c81bf022
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-06-02 08:51:56 +0000

    Create Contact Form model, form, view, template and URLS within 'contactus' app directory

--- contactus/models.py
+++ contactus/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """Schema for the Contact model, to be used for a contact form"""
+    fname = models.CharField(max_length=150)
+    lname = models.CharField(max_length=150)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"New message from {self.fname} {self.lname}: {self.body}"
--- contactus/views.py
+++ contactus/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+from django.contrib import messages
+from .forms import ContactForm
+
+
+class Contact(View):
+    """ View to allow sending of messages """
+    def get(self, request, *args, **kwargs):
+
+        return render(
+            request,
+            'contact.html',
+            {
+                'contact_form': ContactForm()
+            }
+        )
+
+    def post(self, request, *args, **kwargs):
+
+        contact_form = ContactForm(data=request.POST)
+
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Thanks for your message!')
+
+        else:
+            contact_form = ContactForm()
+            # messages.add_message(request, messages.WARNING,
+            #                      'Message not sent. Please see ' +
+            #                      '"Guidance on submitting messages."')
+
+        return render(
+            request,
+            'contact.html',
+            {
+                'contact_form': ContactForm()
+            }
+        )
