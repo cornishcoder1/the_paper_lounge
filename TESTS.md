c1328e55f4dbba4f7e1e97dd133d2fbc708215b5
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-06-09 16:50:08 +0000

    Perform view unit tests on home, about and add review pages. Document in TESTS.md

---
+++ TESTS.md
@@ -0,0 +1,35 @@
+## Table of contents
+1. [Unit Testing](#unit-testing)
+    - [Views](#views) 
+    - [Forms](#forms)
+- [Validator Testing](#validator-testing)
+    - [Lighthouse](#lighthouse)
+    - [W3C Markup](#w3c-markup)
+    - [W3C-CSS](#w3c-css) 
+    - [PEP8](#pep8)
+- [User Story Testing](#ser-story-testing)
+    - [Admin](#admin)
+    - [Site User](#site-user)
+- [Responsive Testing](#Responsive-Testing)
+***
+
+# Unit Testing
+I have performed unit tests for some page views, and forms.
+
+## Views
+
+- Homepage
+
+![Unit Test for Homepage](./static/unit_tests/home_test.png)
+
+- About page 
+
+![Unit Test for About page](./static/unit_tests/about_test.png)
+
+- Add Review page
+
+![Unit Test for Add Review page](./static/unit_tests/add_review_test.png)
+
+Result: 
+
+![Unit Test results](./static/unit_tests/unit_test_results.png)
---
+++ blog/views_test.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+# Unit Testing - Views
+
+class TestIndexView(TestCase):
+    "Unit test for home page"
+    def test_index_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+
+class AboutView(TestCase):
+    "Unit test for about page"
+    def test_about_page(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
+
+class TestAddReviewView(TestCase):
+    "Unit test for add review page"
+    def test_add_review_page(self):
+        response = self.client.get('/add-review/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_review.html')
+
+
